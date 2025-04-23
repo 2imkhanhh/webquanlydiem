fe9ac070a510cb6d3c23d15eb664019119b8b7d6
VietDepTrai <vietdem6886@gmail.com>
2025-04-23 23:26:18 +0700

    Viet Dep Trai moi ban dang nhap

--- View/login.php
+++ View/login.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Đăng nhập</title>
+  <title>Việt quá đẹp trai mời bạn đăng nhập</title>
 
   <!-- Custom fonts for this template-->
   <link href="bootstraps/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
